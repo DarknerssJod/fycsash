f714c221ccd992c0f38349b307a5b1c1af7489ec
DarknerssJod <146918048+DarknerssJod@users.noreply.github.com>
2023-11-06 09:10:17 +0530

    Add files via upload
    this is a random python program file for my ost exam

---
+++ 6d.py
@@ -0,0 +1,13 @@
+class Person:
+    def __int__(self_name,surname,birthdate,address,telephone,email):
+        self_name=name
+        with open(fname) as f:
+            if bufsize>fsize:
+                bufsize=fsize-1
+                print(bufsize)
+                data=[]
+                while True:
+                    iter+=1
+                    f.seek(iter)
+                    data.extend(f.readline())
+                    
